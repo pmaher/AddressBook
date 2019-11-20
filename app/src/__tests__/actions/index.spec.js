25c519ac35f71949782ac747238e2a964ed87dd1
Admin <patrickemaher@yahoo.com>
2019-11-19 19:24:26 -0800

    Finishing up action creator tests

--- app/src/__tests__/actions/index.spec.js
+++ app/src/__tests__/actions/index.spec.js
@@ -7,8 +7,7 @@ jest.mock('axios');
 
 describe('actions', () => {
     it('should dispatch a fetch all addresses action', async () => {
-        const resp = {data: 1};
-        axios.get.mockResolvedValue(resp);
+        axios.get.mockResolvedValue({data: 1});
         const dispatch = jest.fn();
         await actions.fetchAddresses()(dispatch);
         expect(axios.get).toBeCalledWith('/api/address');
@@ -31,13 +30,45 @@ describe('actions', () => {
     })
 
     it('should dispatch the fetch single address action', async () => {
-        const resp = { data: 2};
-        axios.get.mockResolvedValue(resp);
+        axios.get.mockResolvedValue({ data: 2});
         const dispatch = jest.fn();
         await actions.fetchAddress(11)(dispatch);
         expect(axios.get).toBeCalledWith('/api/address/11');
         expect(dispatch).toBeCalledTimes(1);
         expect(dispatch).toBeCalledWith({ type: FETCH_ADDRESS, payload: 2});
+    })
 
+    it('should dispatch the update address action', async () => {
+        const address = { id: 11, firstName: 'Jill' },
+            dispatch = jest.fn(),
+            mockHistory = [];
+        axios.put.mockResolvedValue({ data: address });
+        await actions.updateAddress(address, mockHistory)(dispatch);
+        expect(axios.put).toBeCalledWith('/api/address/11', address);
+        expect(dispatch).toBeCalledTimes(1);
+        expect(dispatch).toBeCalledWith({ type: UPDATE_ADDRESS, payload: address});
+        //signifies going back to the homepage
+        expect(mockHistory).toContain('/');
+    })
+
+    it('should dispatch the create address action', async () => {
+        const address = { id: 22, firstName: 'John' },
+            dispatch = jest.fn(),
+            mockHistory = [];
+        axios.post.mockResolvedValue({ data: address });
+        await actions.createAddress(address, mockHistory)(dispatch);
+        expect(axios.post).toBeCalledWith('/api/address/', address);
+        expect(dispatch).toBeCalledTimes(1);
+        expect(dispatch).toBeCalledWith({ type: CREATE_ADDRESS, payload: address});
+        //signifies going back to the homepage
+        expect(mockHistory).toContain('/');
+    })
+
+    it('should dispatch the delete address action', async () => {
+        const dispatch = jest.fn();
+        await actions.deleteAddress(11)(dispatch);
+        expect(axios.delete).toBeCalledWith('/api/address/11');
+        expect(dispatch).toBeCalledTimes(1);
+        expect(dispatch).toBeCalledWith({ type: DELETE_ADDRESS, payload: { addressId: 11 }});
     })
 })
