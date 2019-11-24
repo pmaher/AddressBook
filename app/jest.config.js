e8b4ee8775ef9a65758c9e6992b8e84415cde6d9
Admin <patrickemaher@yahoo.com>
2019-11-23 16:20:51 -0800

    Minor test cleanup to remove test helpers from execution

--- app/jest.config.js
+++ app/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     "moduleNameMapper": {
         "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
         "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
-      }
+    },
+    "testPathIgnorePatterns" : ["<rootDir>/src/__tests__/helpers/" ]
 }
